83427a152fbd4f3c03eb00aa03588c94a6d8cb53
Sandeep Chanura Abeykoon <sandeepchanura@gmail.com>
2023-03-30 11:58:51 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
-
-import 'package:salescast/Screens/login.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:salescast/Screens/main_page.dart';
-import 'package:salescast/Screens/profile_screen.dart';
-import 'package:salescast/Screens/view_product.dart';
-import 'package:salescast/Screens/welcome.dart';
-import 'package:salescast/Screens/welcome_page_slide.dart';
 import 'package:salescast/auth_service.dart';
 
-import 'Screens/login.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,16 +11,13 @@ void main() async{
 
   runApp(
     DevicePreview(
-      builder: (context) => MyApp(), // Wrap your app
+      builder: (context) => const MyApp(), // Wrap your app
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-
-
-
-
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
@@ -41,17 +29,12 @@ class MyApp extends StatelessWidget {
 
       theme: ThemeData(
 
-
       ),
        home:
         AuthService().handleAuthState()
 
-
     );
 
   }
-
-
-
 }
 
