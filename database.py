50bfaee1dcf811250766b8bb193267542031b3a9
Dananjayan <107125445+AkilanD04@users.noreply.github.com>
2023-03-30 20:40:33 +0530

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ def database_add(records):
     first = records[0]
     print(first)
     client = pymongo.MongoClient(
-        'mongodb+srv://admin:admin123@cluster0.xtwmxll.mongodb.net/?retryWrites=true&w=majority',
+        'mongodb+srv://admin:admin123@cluster0.qva0hbp.mongodb.net/?retryWrites=true&w=majority',
         ssl=True)
     if client:
         print("connected successfully to database")
@@ -21,10 +21,10 @@ def database_add(records):
 
 def product_available(user_id,product_id,is_available):
     client = pymongo.MongoClient(
-        'mongodb+srv://admin:admin123@cluster0.xtwmxll.mongodb.net/?retryWrites=true&w=majority',
+        'mongodb+srv://admin:admin123@cluster0.qva0hbp.mongodb.net/?retryWrites=true&w=majority',
         ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
     db = client["SalesCast"]
-    collection = db["Sales_records"]
+    collection = db["Sales_record"]
     results = collection.find_one({"userid": user_id, "productID": product_id})
     if collection.count_documents({}) != 0:
         if results:
@@ -41,10 +41,10 @@ def product_available(user_id,product_id,is_available):
 def add_product(user_id,product_name,product_id,product_price,product_brand,product_category):
     print("inside db")
     client = pymongo.MongoClient(
-        'mongodb+srv://admin:admin123@cluster0.xtwmxll.mongodb.net/?retryWrites=true&w=majority',
+        'mongodb+srv://admin:admin123@cluster0.qva0hbp.mongodb.net/?retryWrites=true&w=majority',
         ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
     db = client["SalesCast"]
-    collection = db["Sales_records"]
+    collection = db["Sales_record"]
     post = {"userid": user_id, "productName": product_name, "productID": product_id, "product price": product_price,"product category": product_category, "product brand": product_brand}
     collection.insert_one(post)
     print("added succesfully")
