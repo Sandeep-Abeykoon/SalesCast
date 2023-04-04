d29c80a1fa82e676ba8b47cee03f9bbfca1a2bdd
RaviyaLK <103655504+RaviyaLK@users.noreply.github.com>
2023-04-04 09:19:16 +0530

    Did some changes

--- Font-end/lib/Screens/view_product.dart
+++ Font-end/lib/Screens/view_product.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-
-
-
-
+import 'package:salescast/reusable_widget/app_large_text.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class ViewProduct extends StatefulWidget {
   ViewProduct({Key? key}) : super(key: key);
@@ -10,58 +8,119 @@ class ViewProduct extends StatefulWidget {
   @override
   State<ViewProduct> createState() => _ViewProductState();
 }
+var productname="";
+var productPrice="";
+var productId=" ";
+var productCategory="";
+var productBrand ="";
+var productImageUrl="";
+List<int> trends = [1,2,3];
 
 class _ViewProductState extends State<ViewProduct> {
+
   @override
   Widget build(BuildContext context) {
     List<_SalesData> data = [
-      _SalesData('Monday', 10,),
-      _SalesData('Tuesday', 18),
-      _SalesData('Wednesday', 24),
-      _SalesData('Thursday', 12),
-      _SalesData('Friday', 16),
-      _SalesData('Saturday', 11),
-      _SalesData('Sunday', 10),
+      _SalesData('Week 1', 10,),
+      _SalesData('Week 2', 18),
+      _SalesData('Week 3', 24),
+      _SalesData('Week 4', 12),
 
     ];
     final List<ChartData> chartData = <ChartData>[
-      ChartData('Monday', 128, 129),
-      ChartData('Tuesday', 123, 92),
-      ChartData('Wednesday', 107, 106),
-      ChartData('Thursday', 100, 85),
-      ChartData('Friday', 87, 95),
-      ChartData('Saturday', 71, 65),
-      ChartData('Sunday', 58, 105),
+      ChartData('Week 1', 128, 129),
+      ChartData('Week 2', 123, 92),
+      ChartData('Week 3', 107, 106),
+      ChartData('Week 4', 87, 95),
     ];
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.grey.withOpacity(0.3),
-        title: Text("Product Name",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 18),),
+        title: Text(
+          "Product Name",
+          style: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),
+        ),
         actions: [
           PopupMenuButton(
-
-
-
-
-              itemBuilder:(context)=>[
-                PopupMenuItem(child:Text("Delete"),
-                onTap: (){
-                  //code to delete item
-
-
-                },),
+              itemBuilder: (context) => [
+                PopupMenuItem(
+                  child: Text("Delete"),
+                  onTap: () {
+                    //code to delete item
+                  },
+                ),
               ]),
         ],
       ),
-       body:  SingleChildScrollView(
-             child: Container(
-                 child: Column(
-                   children: [
-                  Container(
-                  child: Padding(
-                      padding: EdgeInsets.all(16),
-                         child: Image.asset("lib/assets/images/Electronics.png",height: 200,width: 300,),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Image.asset(
+                    "lib/assets/images/Electronics.png",
+                    height: 200,
+                    width: 300,
+                  ),
                 ),
+              ),
+              // Divider(thickness: 5,),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                height: 1500,
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(35),
+                      topRight: Radius.circular(35),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.4),
+                        blurRadius: 10,
+                        spreadRadius: 1,
+                      ),
+                    ]),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "Samsung TV",
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Divider(
+                      thickness: 1,
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Product ID: 20",
+                          style: TextStyle(fontSize: 20, color: Colors.black,fontWeight: FontWeight.w300),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
 
                     Row(
                       children: [
@@ -104,19 +163,18 @@ class _ViewProductState extends State<ViewProduct> {
                           for (var i in trends)Padding(
                             padding: EdgeInsets.all(10),
                             child: GestureDetector(
-                              onTap: (){
+                                onTap: (){
 
 
-                              },
+                                },
                                 child: Text("$i. JVC LT-40CA790 Android TV 40' Smart Full HD LED TV with Google Assistant",style: TextStyle(fontSize: 15,color: Colors.blue),)),
                           ),
 
                         ],
                       ),
                     )
-                    , SizedBox(height: 70,),
+                    , SizedBox(height: 50,),
                     AppLargeText(text: "Forecast"),
-                    SizedBox(height: 40,),
                     Column(
                         children:[
                           Padding(
@@ -124,7 +182,7 @@ class _ViewProductState extends State<ViewProduct> {
                             child: SfCartesianChart(
                                 primaryXAxis: CategoryAxis(),
                                 // Chart title
-                                title: ChartTitle(text: 'Next Week sales analysis of the product'),
+                                title: ChartTitle(text: 'Monthly sales analysis of the product'),
                                 // Enable legend
                                 legend: Legend(isVisible: true),
                                 // Enable tooltip
@@ -140,14 +198,13 @@ class _ViewProductState extends State<ViewProduct> {
                                       dataLabelSettings: DataLabelSettings(isVisible: true))
                                 ]),
                           ),
-                          SizedBox(height: 40,),
                           Container(
                               width: 500,
                               height: 300,
                               child: Padding(
                                 padding: EdgeInsets.fromLTRB(0,30, 0, 0),
                                 child: SfCartesianChart(
-                                    title: ChartTitle(text: 'Next Week sales Comparison analysis'),
+                                    title: ChartTitle(text: 'Monthly sales Comparison analysis'),
                                     legend: Legend(isVisible: true, isResponsive: true, ),
                                     primaryXAxis: CategoryAxis(),
                                     tooltipBehavior: TooltipBehavior(enable: true,),
@@ -160,7 +217,7 @@ class _ViewProductState extends State<ViewProduct> {
                                           yValueMapper: (ChartData data, _) => data.y
                                       ),
                                       LineSeries<ChartData, String>(
-                                          name: "Most Trending Item",
+                                          name: "Trending Item",
                                           dataSource: chartData,
                                           xValueMapper: (ChartData data, _) => data.x,
                                           yValueMapper: (ChartData data, _) => data.y1
@@ -172,96 +229,28 @@ class _ViewProductState extends State<ViewProduct> {
                           ),
                         ] ), ],
                 ),
-                     // Divider(thickness: 5,),
-                     SizedBox(height: 40,),
-                     Container(
-                       height: MediaQuery.of(context).size.height,
-                       width: double.infinity,
-                       padding: EdgeInsets.symmetric(vertical: 30,horizontal: 20),
-                       decoration: BoxDecoration(
-                         color: Colors.white,
-                         borderRadius: BorderRadius.only(
-                           topLeft: Radius.circular(35),
-                           topRight: Radius.circular(35),
-                         ),
-                         boxShadow: [BoxShadow(
-                           color: Colors.grey.withOpacity(0.4),
-                           blurRadius: 10,
-                           spreadRadius: 1,
-                         ),]
-                       ),
-                       child: Column(
-                         children: [
-                           Row(
-                             children: [
-                               
-                               Text("Samsung TV",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.grey),)
-
-                           ],
-                           ),
-                           SizedBox(height: 10,),
-                           Divider(thickness: 1,),
-                           SizedBox(height: 30,),
-
-                           Row(
-                             children: [
-                               Text("Product ID: 20",style: TextStyle(fontSize: 20, color: Colors.black38),)
-
-                             ],
-                           ),
-                           SizedBox(height: 10,),
-                           Row(
-                             children: [
-                               Text("Quantity : 20",style: TextStyle(fontSize: 20, color: Colors.black38),)
-
-                             ],
-                           ),
-                           SizedBox(height: 10,),
-                           Row(
-                             children: [
-                               Text("Price : 800",style: TextStyle(fontSize: 20, color: Colors.black38),)
-
-                             ],
-                           ),
-                           SizedBox(height: 10,),
-                           Row(
-                             children: [
-                               Text("Product Category : Electronics",style: TextStyle(fontSize: 20, color: Colors.black38),)
-
-                             ],
-                           ),
-                           SizedBox(height: 10,),
-                           Row(
-                             children: [
-                               Text("Product Brand : Samsung",style: TextStyle(fontSize: 20, color: Colors.black38),)
-
-                             ],
-                           ),
-
-
-
-
-
-
-
-
-                         ],
-                       ),
-
-
-
-
-
-                     ),
-
-             ],
-
-         ),
+              ),
+            ],
+          ),
         ),
-    ) ,
-
-
-
+      ),
     );
   }
+}
+class ChartData {
+  ChartData(this.x, this.y, this.y1);
+  final String x;
+  final double? y;
+  final double? y1;
+
+}
+
+
+
+class _SalesData {
+  _SalesData(this.year, this.sales);
+
+
+  final String year;
+  final double sales;
 }
