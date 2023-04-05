2c9b122c887ea4255c997d70f3707ff1d72d5612
Sandeep Chanura Abeykoon <sandeepchanura@gmail.com>
2023-04-05 14:55:49 +0530

    updated the code

--- Font-end/lib/Screens/forecasts.dart
+++ Font-end/lib/Screens/forecasts.dart
@@ -14,10 +14,7 @@ class _ForecastsPageState extends State<ForecastsPage> {
   @override
   Widget build(BuildContext context) {
     List<_SalesData> data = [
-      _SalesData(
-        'Product 1',
-        10,
-      ),
+      _SalesData('Product 1', 10,),
       _SalesData('Product 2', 18),
       _SalesData('Product 3', 0),
       _SalesData('Product 4', 12),
@@ -151,51 +148,6 @@ class _ForecastsPageState extends State<ForecastsPage> {
                                   DataCell(Text('30')),
                                 ],
                               ),
-                            // DataRow(
-                            //   cells: <DataCell>[
-                            //     DataCell(Text('Product C')),
-                            //     DataCell(Text('20')),
-                            //     DataCell(Text('Trending Product C')),
-                            //     DataCell(Text('30')),
-                            //
-                            //   ],
-                            // ),
-                            // DataRow(
-                            //   cells: <DataCell>[
-                            //     DataCell(Text('Product D')),
-                            //     DataCell(Text('20')),
-                            //     DataCell(Text('Trending Product D')),
-                            //     DataCell(Text('30')),
-                            //
-                            //   ],
-                            // ),
-                            // DataRow(
-                            //   cells: <DataCell>[
-                            //     DataCell(Text('Product E')),
-                            //     DataCell(Text('20')),
-                            //     DataCell(Text('Trending Product E')),
-                            //     DataCell(Text('30')),
-                            //
-                            //   ],
-                            // ),
-                            // DataRow(
-                            //   cells: <DataCell>[
-                            //     DataCell(Text('Product F')),
-                            //     DataCell(Text('20')),
-                            //     DataCell(Text('Trending Product F')),
-                            //     DataCell(Text('30')),
-                            //
-                            //   ],
-                            // ),
-                            // DataRow(
-                            //   cells: <DataCell>[
-                            //     DataCell(Text('Product G')),
-                            //     DataCell(Text('20')),
-                            //     DataCell(Text('Trending Product G')),
-                            //     DataCell(Text('30')),
-                            //
-                            //   ],
-                            // ),
                           ],
                         ),
                       ),
@@ -239,7 +191,7 @@ class _ForecastsPageState extends State<ForecastsPage> {
                           // Chart title
                           title: ChartTitle(
                               text:
-                              'Next week sales forecast of your product'),
+                              'Next week sales forecast of your products'),
                           // Enable legend
                           legend: Legend(isVisible: true),
                           // Enable tooltip
@@ -248,7 +200,7 @@ class _ForecastsPageState extends State<ForecastsPage> {
                             BarSeries<_SalesData, String>(
                                 dataSource: data,
                                 xValueMapper: (_SalesData sales, _) =>
-                                sales.year,
+                                sales.week,
                                 yValueMapper: (_SalesData sales, _) =>
                                 sales.sales,
                                 name: 'Sales',
@@ -274,7 +226,7 @@ class _ForecastsPageState extends State<ForecastsPage> {
                                 color: Colors.redAccent,
                                 dataSource: data2,
                                 xValueMapper: (_SalesData sales, _) =>
-                                sales.year,
+                                sales.week,
                                 yValueMapper: (_SalesData sales, _) =>
                                 sales.sales,
                                 name: 'Sales',
@@ -291,104 +243,6 @@ class _ForecastsPageState extends State<ForecastsPage> {
         ),
       ),
     );
-
-    //
-    // //   Scaffold(
-    // //   backgroundColor: Colors.white,
-    // //   appBar: AppBar(
-    // //     title: Text(
-    // //       "Forecasts",
-    // //       style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-    // //     ),
-    // //     backgroundColor: Colors.transparent,
-    // //     elevation: 0,
-    // //   ),
-    //   body: SingleChildScrollView(
-    //     child: Container(
-    //       child: Column(
-    //         children: [
-    //           Container(
-    //             height: 200,
-    //             width: MediaQuery.of(context).size.width,
-    //             margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-    //             padding: EdgeInsets.all(10),
-    //             decoration: BoxDecoration(
-    //               boxShadow: [
-    //                 BoxShadow(
-    //                   color: Colors.grey.withOpacity(0.1),
-    //                   blurRadius: 8,
-    //                   offset: Offset(0, 5),
-    //                 ),
-    //               ],
-    //               color: Colors.grey.withOpacity(0.2),
-    //               borderRadius: BorderRadius.circular(10),
-    //             ),
-    //             child: Padding(
-    //               padding: EdgeInsets.all(15),
-    //               child: const Text(
-    //                 "Hey! \n\nYou Don't have any Forecasts yet 🥺. Try adding product sales records and come back  ",
-    //                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
-    //               ),
-    //             ),
-    //           ),
-    //           Padding(
-    //             padding: EdgeInsets.only(left: 30),
-    //             child: SfCartesianChart(
-    //                 primaryXAxis: CategoryAxis(),
-    //                 // Chart title
-    //                 title: ChartTitle(text: 'Next week sales forecast of your product'),
-    //                 // Enable legend
-    //                 legend: Legend(isVisible: true),
-    //                 // Enable tooltip
-    //                 tooltipBehavior: TooltipBehavior(enable: true),
-    //                 series: <ChartSeries<_SalesData, String>>[
-    //                   BarSeries<_SalesData, String>(
-    //                       dataSource: data,
-    //                       xValueMapper: (_SalesData sales, _) => sales.year,
-    //                       yValueMapper: (_SalesData sales, _) => sales.sales,
-    //
-    //                       name: 'Sales',
-    //                       // Enable data label
-    //                       dataLabelSettings: DataLabelSettings(isVisible: true))
-    //                 ]),
-    //           ),
-    //           Padding(
-    //             padding: EdgeInsets.fromLTRB(30,30, 0, 20),
-    //             child: SfCartesianChart(
-    //
-    //                 primaryXAxis: CategoryAxis(),
-    //                 // Chart title
-    //                 title: ChartTitle(text: 'Next week sales forecast of most trending product'),
-    //                 // Enable legend
-    //                 legend: Legend(isVisible: true),
-    //                 // Enable tooltip
-    //                 tooltipBehavior: TooltipBehavior(enable: true),
-    //                 series: <ChartSeries<_SalesData, String>>[
-    //                   BarSeries<_SalesData, String>(
-    //                     color: Colors.redAccent,
-    //                       dataSource: data2,
-    //                       xValueMapper: (_SalesData sales, _) => sales.year,
-    //                       yValueMapper: (_SalesData sales, _) => sales.sales,
-    //
-    //                       name: 'Sales',
-    //                       // Enable data label
-    //                       dataLabelSettings: DataLabelSettings(isVisible: true))
-    //                 ]),
-    //           ),
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //         ],
-    //       ),
-    //     ),
-    //   ),
-    // );
   }
 }
 
@@ -400,8 +254,8 @@ class ChartData {
 }
 
 class _SalesData {
-  _SalesData(this.year, this.sales);
+  _SalesData(this.week, this.sales);
 
-  final String year;
+  final String week;
   final double sales;
 }
